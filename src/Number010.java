d0c1fe9b2c21e0708721a2f89e88122508d12d6d
Finn183747 <usmanovakbar47@gmail.com>
2022-09-05 21:24:08 +0500

    Number-010

--- src/Number010.java
+++ src/Number010.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Number010 {
+import java.util.Scanner;
+
+public class Number010 {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = a / 10;
+        System.out.println(b % 10);
+    }
 }
