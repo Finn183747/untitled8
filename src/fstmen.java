39edc37801a9ecff6ea8154b8f98753629d56567
Finn183747 <usmanovakbar47@gmail.com>
2022-09-13 19:25:09 +0500

    fstm

---
+++ src/fstmen.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class fstmen {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+
+        if (a == b && b ==  c && a == c) {
+            System.out.println(a);
+        }
+        if (a > b && a > c) {
+            if (a <= 727 && a >= 94) {
+                System.out.println(a);
+            } else {
+                System.out.println("ERROR");
+            }
+        } else if (b > a && b > c) {
+            if (b <= 727 && b >= 94) {
+                System.out.println(b);
+            } else {
+                System.out.println("ERROR");
+            }
+        } else if (c > a && c > b) {
+            if (c <= 727 && c>= 94) {
+                System.out.println(c);
+            } else {
+                System.out.println("ERROR");
+            }
+        }
+    }
+}
